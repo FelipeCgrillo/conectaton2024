af793c83473d14f3aa89161a372477843bdf686c
Celina <celina.seelinger@med.uni-heidelberg.de>
2024-11-25 09:58:37 +0100

    Removed not used imports

--- pages/laboratory.py
+++ pages/laboratory.py
@@ -2,8 +2,6 @@ import streamlit as st
 from menu import menu_with_redirect
 import pandas as pd
 import plotly.express as px
-import requests
-from fhir.resources.composition import Composition
 
 patient_id = st.session_state.get('patient_id', None)
 timeline_data = st.session_state.get('laboratory_data', None)
